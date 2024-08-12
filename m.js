971be750ff16fb2f46be0f80f17efc91fff4cb4a
Irvinworksz <delacruzjohnirvin05@gmail.com>
2024-08-12 20:32:57 +0800

    Add files via upload

---
+++ m.js
@@ -0,0 +1,4 @@
+document.getElementById('revealButton').addEventListener('click', function() {
+    var hiddenImage = document.getElementById('hiddenImage');
+    hiddenImage.classList.toggle('show');
+});
