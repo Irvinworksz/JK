541e1b94d677f3856b61e4596c79ffb92951e52e
Irvinworksz <delacruzjohnirvin05@gmail.com>
2024-08-12 20:21:06 +0800

    Add files via upload

---
+++ k.js
@@ -0,0 +1,4 @@
+document.getElementById('clickableText').addEventListener('click', function() {
+    var hiddenText = document.getElementById('hiddenText');
+    hiddenText.classList.toggle('show');
+});
